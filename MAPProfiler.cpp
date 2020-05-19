19a6000fd4da3c7cfb21688eb23e9a5713a6d66b
Alif Ahmed <alifahmed@virginia.edu>
2020-05-19 14:38:28 -0400

    Loop compression works

--- MAPProfiler.cpp
+++ MAPProfiler.cpp
@@ -21,6 +21,9 @@
 #include <set>
 #include <vector>
 #include <algorithm>
+#include <stack>
+#include <cassert>
+#include <sstream>
 #include "pin.H"
 
 using namespace std;
@@ -68,33 +71,9 @@ KNOB<bool> knobWrite(KNOB_MODE_WRITEONCE, "pintool", "write", "1",
 /*******************************************************************************
  * Structs
  ******************************************************************************/
-#define LINE_SIZE   64
+struct InsBase;
 
-// Structure for keeping thread specific data. Padded to LINE_SIZE for avoiding
-// false sharing.
-typedef struct {
-  // Tracks if the thread is inside the requested functions. Greater than 0
-  // means it is.
-  UINT64 rtnEntryCnt;
-
-  // padding
-  UINT8 _padding[LINE_SIZE - sizeof(UINT64)];
-} ThreadData;
-
-// Keeps information for each memory access. Padded to LINE_SIZE for avoiding
-// false sharing.
-typedef struct {
-  // Effective virtual address
-  ADDRINT ea;
-
-  // Type of access. 'R' for read and 'W' for write.
-  UINT8 type;
-
-  // padding
-  UINT8 _padding[LINE_SIZE - sizeof(ADDRINT) - sizeof(UINT8)];
-} MemInfo;
-
-typedef struct {
+typedef struct InstInfo{
   string dis;
   UINT64 totalSz = 0;
   UINT64 cnt = 0;
@@ -104,21 +83,42 @@ typedef struct {
   INT64 totalStride = 0;
   ADDRINT startAddr = -1;
   ADDRINT endAddr = 0;
+  ADDRINT instAddr = -1;
 } InstInfo;
+
+typedef struct InsLoop {
+  UINT64 loopCnt = 0;
+  vector<InsBase*> ins;
+}InsLoop;
+
+typedef struct InsBlock{
+  UINT64 id = -1;
+  bool isTop = false;
+  map<InsBlock*, UINT64> outEdges;   //successors and count
+  //map<InsBlock*, UINT64> inEdges;    //predecessors and count
+  set<InsBlock*> inEdges;
+  vector<InsBase*> ins;
+  //vector<InstInfo*> ins;
+} InsBlock;
+
+typedef enum {InsTypeInvalid, InsTypeNormal, InsTypeLoop} InsType;
+
+typedef struct InsBase{
+  InsType type = InsTypeInvalid;
+  //InstInfo* insNormal = NULL;
+  string insNormal = "";
+  InsLoop* insLoop = NULL;
+}InsBase;
+
+map<ADDRINT, InsBlock*> insAddrToBlk;
+vector<ADDRINT> insTrace;
+vector<InsLoop*> loops;
+vector<InsBase*> bases;
 
 typedef map<ADDRINT, InstInfo*> InfoMap;
 
 InfoMap rInfo;
 InfoMap wInfo;
-
-/*******************************************************************************
- * Globals
- ******************************************************************************/
-// Array that contains all the memory accesses.
-MemInfo *info;
-
-// Array that keeps thread specific data for all threads.
-ThreadData *tdata;
 
 // Limit of buffer entry. Maps to knobMaxLog.
 UINT64 buf_log_lim;
@@ -175,8 +175,8 @@ void record(InstInfo* info, ADDRINT ea, UINT32 sz) {
   }
   info->lastEA = ea;
   info->cnt++;
+  insTrace.push_back(info->instAddr);
 }
-
 
 InstInfo* checkIns(INS &ins, InfoMap &im) {
   ADDRINT addr = INS_Address(ins);
@@ -185,7 +185,20 @@ InstInfo* checkIns(INS &ins, InfoMap &im) {
     InstInfo* info = new InstInfo();
     //info->ins = ins;
     info->dis = INS_Disassemble(ins);
+    info->instAddr = INS_Address(ins);
     im[addr] = info;
+    if(insAddrToBlk.find(addr) == insAddrToBlk.end()){
+      static UINT64 id = 2;
+      InsBlock* ib = new InsBlock();
+      InsBase* base = new InsBase();
+      ib->id = id++;
+      base->type = InsType::InsTypeNormal;
+      base->insNormal = info->dis;
+      //ib->ins.push_back(base);
+      ib->ins.push_back(base);
+      bases.push_back(base);
+      insAddrToBlk[addr] = ib;
+    }
   }
   return im[addr];
 }
@@ -234,6 +247,260 @@ VOID RtnEntry(THREADID tid) {
 VOID RtnLeave(THREADID tid) {
   //tdata[tid].rtnEntryCnt--;
   rtnEntryCnt--;
+}
+
+
+typedef struct BblInfo {
+  UINT64 id = -1;
+  //UINT64 count = 0;
+  bool isUsing = true;
+  bool entered = false;
+  map<BblInfo*, UINT64> outEdges;   //successors and count
+  map<BblInfo*, UINT64> inEdges;    //predecessors and count
+  map<ADDRINT, bool> insMap;
+} BblInfo;
+
+map<ADDRINT, UINT64>  bblAddrIdMap;
+map<UINT64, BblInfo*> bblIdInfoMap;
+vector<UINT64> bblTrace;
+vector<UINT64> bblTraceTop;
+
+VOID BblEntry(UINT64 bi) {
+  if(rtnEntryCnt > 0){
+    bblTrace.push_back(bi);
+  }
+}
+
+UINT64 getBBLId(BBL bbl){
+  static UINT64 id = 1;
+  ADDRINT addr = BBL_Address(bbl);
+  if(bblAddrIdMap.find(addr) == bblAddrIdMap.end()){
+    //create new object
+    BblInfo* bi = new BblInfo();
+    bi->id = id++;
+    bblAddrIdMap[addr] = bi->id;
+    bblIdInfoMap[id] = bi;
+    for(INS ins = BBL_InsHead(bbl); ins != BBL_InsTail(bbl); ins = INS_Next(ins)){
+      if(INS_Valid(ins)){
+        bi->insMap[INS_Address(ins)] = true;
+      }
+    }
+    bi->insMap[INS_Address(BBL_InsTail(bbl))] = true;
+    assert(bi->insMap.size() == BBL_NumIns(bbl));
+  }
+  return bblAddrIdMap[addr];
+}
+
+VOID TrcCallback(TRACE trace, VOID *arg){
+  for( BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl) ){
+    UINT64 bi = getBBLId(bbl);
+    BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR) BblEntry, IARG_UINT64, bi, IARG_END);
+  }
+}
+
+
+bool compressDCFG(set<InsBlock*> &cfg){
+  bool isChanged = false;
+  bool merged;
+  do{
+    merged = false;
+    for(InsBlock* in : cfg){
+      if(in->id == 0) continue;   //skip begin block
+      if(in->outEdges.size() == 1){
+        InsBlock* out = in->outEdges.begin()->first;
+        if(out->id == 1) continue;
+        if(out->inEdges.size() == 1){
+          //merge in with out
+          //assert(out->inEdges.begin()->first == in);
+
+          //replace out edges
+          in->outEdges = out->outEdges;
+
+          //copy instructions
+          in->ins.insert(in->ins.end(), out->ins.begin(), out->ins.end());
+
+          //mark to be deleted
+          merged = true;
+          isChanged = true;
+          cfg.erase(out);
+          break;
+        }
+      }
+    }
+  } while(merged);
+  return isChanged;
+}
+
+bool compressLoop(set<InsBlock*> &cfg){
+  bool isChanged = false;
+  bool merged;
+  do{
+    merged = false;
+    for(InsBlock* in : cfg){
+      //check conditions for inferring as a loop
+      map<InsBlock*, UINT64>::iterator ed = in->outEdges.find(in);
+      if(ed != in->outEdges.end()){
+        //most probably found a loop
+
+        //get loop count
+        UINT64 m = ed->second;
+        UINT64 total = 0;
+        for(auto e : in->outEdges){
+          total += e.second;
+        }
+        UINT64 k = total - m;
+        if(total % k) continue;   //skip if not divisible
+        UINT64 loopCnt = total / k;
+        //remove outgoing loop edge
+        in->outEdges.erase(ed);
+        //remove incoming loop edge (just removing any edge is fine for now, since we are not using exact edges, just counts)
+        in->inEdges.erase(in->inEdges.begin());
+
+        InsLoop* loop = new InsLoop();
+        loops.push_back(loop);
+        InsBase* base = new InsBase();
+        bases.push_back(base);
+
+        loop->loopCnt = loopCnt;
+        loop->ins = in->ins;    //copy
+        in->ins.clear();
+
+        base->type = InsType::InsTypeLoop;
+        base->insLoop = loop;
+        in->ins.push_back(base);
+
+        //mark
+        merged = true;
+        isChanged = true;
+        break;
+      }
+    }
+  } while(merged);
+  return isChanged;
+}
+
+void mergeAllLoops(set<InsBlock*> &cfg){
+  bool isChanged;
+  do{
+    compressDCFG(cfg);
+    isChanged = compressLoop(cfg);
+  } while(isChanged);
+}
+
+bool firstLine = true;
+
+void printInsLabel(ofstream &out, const vector<InsBase*> &insts, UINT32 indent){
+  for(InsBase* it : insts){
+    if(!firstLine){
+      out << "<br />";
+    }
+    firstLine = false;
+    for(UINT32 i = 0; i < indent; i++){
+      out << " &nbsp; &nbsp; ";
+    }
+    if(it->type == InsType::InsTypeNormal){
+      out << it->insNormal;
+      //out << "\\n";
+    }
+    else if(it->type == InsType::InsTypeLoop){
+      InsLoop* loop = it->insLoop;
+      out << "Loop: " << loop->loopCnt;
+      //out << "\\n";
+      printInsLabel(out, loop->ins, indent+1);
+    }
+  }
+}
+
+void printDot(const set<InsBlock*> &cfg, const char* fname){
+  ofstream out(fname);
+  cout << "Printing " << fname << "...\n";
+
+  out << "digraph DCFG {\n";
+
+  out << "\tB0 [label=\"START\",fillcolor=yellow,style=filled];\n";
+  out << "\tB1 [label=\"END\",fillcolor=yellow,style=filled];\n";
+
+  for(InsBlock* ib : cfg){
+    if(ib->id >= 2){
+      out << "\tB" << ib->id << " [shape=plain, label=< <table>";
+      out << "<TR><TD balign=\"left\" border=\"0\">";
+      firstLine = true;
+      printInsLabel(out, ib->ins, 0);
+      out << "</TD></TR> </table> >];\n";
+    }
+    for(auto it : ib->outEdges){
+      InsBlock* o = it.first;
+      out << "\tB" << ib->id;// << ":o" << cnt;
+      out << " -> B" << o->id;// << ":i";
+      out << " [label=\"" << it.second << "\"];\n";
+    }
+  }
+
+  out << "}\n";
+}
+
+void generateInst(ofstream &out, InsBlock* ins){
+
+}
+
+void generateCode(set<InsBlock*> &cfg, const char* fname){
+  //find root node
+  InsBlock* root = NULL;
+  for(InsBlock* ins : cfg){
+    if(ins->id == 0){
+      root = ins;
+      break;
+    }
+  }
+  assert(root != NULL);
+
+  //open output file
+  ofstream out(fname);
+
+  //write headers
+
+  //write code blocks
+  generateInst(out, root);
+
+  //write footer
+
+}
+
+set<InsBlock*> createDCFGfromInstTrace(vector<ADDRINT> &trace){
+  /*for(auto it : insAddrToBlk){
+    it.second->entered = false;
+    it.second->inEdges.clear();
+    it.second->outEdges.clear();
+  }*/
+
+  //generate edges and edge count for each top instructions
+  set<InsBlock*> topInsBlocks;
+  InsBlock* prev = NULL;
+  UINT64 ttCnt = 0;
+  for(ADDRINT addr : trace){
+    InsBlock* ib = insAddrToBlk[addr];
+    if(ib->isTop){
+      topInsBlocks.insert(ib);
+      ttCnt++;
+      if(prev != NULL){
+        //add bi as the successor of prev
+        //ib->inEdges[prev]++;
+        ib->inEdges.insert(prev);
+        prev->outEdges[ib]++;
+      }
+      prev = ib;
+    }
+  }
+  cout << "Top trace size: " << ttCnt << endl;
+  printDot(topInsBlocks, "dcfgBC.gv");
+
+  compressDCFG(topInsBlocks);
+  printDot(topInsBlocks, "dcfgAC.gv");
+
+  mergeAllLoops(topInsBlocks);
+  printDot(topInsBlocks, "dcfgLC.gv");
+
+  return topInsBlocks;
 }
 
 /**
@@ -352,15 +619,16 @@ UINT64 getTotalAccessSz(InfoMap &mp){
 }
 
 void printInfo(const vector<InstInfo*> &res){
-  for(auto i : res){
+  for(InstInfo* i : res){
+    cout << setw(20) << hex << i->instAddr << dec << "\t";
     cout << setw(50) << dec << i->dis;
-    cout << setw(20) << "TotalSz: " << i->totalSz << "\t\t";
-    cout << setw(20) << "Count: " << i->cnt << "\t\t";
-    cout << setw(20) << "AvgStride: " << (double)(i->totalStride) / (double)(i->cnt) << "\t\t";
-    cout << setw(20) << "Same Stride Cnt: " << i->sameStrideCnt << "\t\t";
-    cout << setw(20) << "Start Addr: " << hex << i->startAddr << "\t\t";
-    cout << setw(20) << "End Addr: " << hex << i->endAddr << "\t\t";
-    cout << endl;
+    //cout << setw(20) << "TotalSz: " << i->totalSz << "\t\t";
+    cout << setw(20) << "Count: " << i->cnt << "\t";
+    cout << setw(20) << "AvgStride: " << (double)(i->totalStride) / (double)(i->cnt) << "\t";
+    cout << setw(20) << "Same Stride Cnt: " << i->sameStrideCnt << "\t";
+    cout << setw(20) << "Start Addr: " << hex << i->startAddr << "\t";
+    cout << setw(20) << "End Addr: " << hex << i->endAddr << "\t";
+    cout << dec << endl;
     delete i;
   }
 }
@@ -384,14 +652,15 @@ vector<InstInfo*> keepTop(InfoMap &mp, double perc){
   });
 
   sz = 0;
-  for(auto it : top){
+  for(InstInfo* it : top){
     res.push_back(it);
+    insAddrToBlk[it->instAddr]->isTop = true;
     sz += it->totalSz;
     if(sz > szAfter)  break;
   }
 
   sz = 0;
-  for(auto it: res){
+  for(InstInfo* it: res){
     sz += it->totalSz;
   }
   cout << "After pruning. NumInst: " << res.size() << "\t\tAccessSz: " << sz << endl;
@@ -404,8 +673,55 @@ void deleteInfoMap(InfoMap &mp){
   }
 }
 
+void deleteBblMap(){
+  for(auto it : bblIdInfoMap){
+    delete it.second;
+  }
+}
+
+void deleteInsBlockMap(){
+  for(auto it : insAddrToBlk){
+    delete it.second;
+  }
+}
+
+void deleteLoops(){
+  for(InsLoop* i : loops){
+    delete i;
+  }
+}
+
+void deleteBases(){
+  for(InsBase* i : bases){
+    delete i;
+  }
+}
+
+void printBblInfo(UINT64 id){
+  BblInfo* bi = bblIdInfoMap[id];
+  cout << "id: " << id;
+  cout << "\tisUsing: " << bi->isUsing;
+  cout << "\tentered: " << bi->entered << endl;
+  for(auto it : bi->insMap){
+    cout << "\t" << hex << it.first << dec << endl;
+    if(rInfo.find(it.first) != rInfo.end()){
+      cout << "\t" << rInfo[it.first]->dis << endl;
+    }
+    if(wInfo.find(it.first) != wInfo.end()){
+      cout << "\t" << wInfo[it.first]->dis << endl;
+    }
+  }
+}
+
 
 VOID Fini(INT32 code, VOID *v) {
+  // add finish trace to insTrace
+  InsBlock* end = new InsBlock();
+  end->id = 1;
+  end->isTop = true;
+  insTrace.push_back(1);
+  insAddrToBlk[1] = end;
+
   // 0. Filter zero  (or very low) cnt instructions
   deleteZeroAccesses(rInfo);
   deleteZeroAccesses(wInfo);
@@ -420,12 +736,46 @@ VOID Fini(INT32 code, VOID *v) {
   //cout << "Read size: " << rdSz << endl;
   //cout << "Write size: " << wrSz << endl;
 
+  //createDCFGfromTrace(bblTrace);
+
   // 3. Only take instructions that represents 90% of reads/writes
   auto rRes = keepTop(rInfo, 95);
   auto wRes = keepTop(wInfo, 95);
 
+  //mark BBL
+  /*for(auto it : bblIdInfoMap){
+    BblInfo* bi = it.second;
+    bi->isUsing = false;
+    for(InstInfo* rr : rRes){
+      ADDRINT addr = rr->instAddr;
+      if(bi->insMap.find(addr) != bi->insMap.end()){
+        bi->isUsing = true;
+        break;
+      }
+    }
+    if(bi->isUsing == false){
+      for(InstInfo* ww : wRes){
+        ADDRINT addr = ww->instAddr;
+        if(bi->insMap.find(addr) != bi->insMap.end()){
+          bi->isUsing = true;
+          break;
+        }
+      }
+    }
+  }*/
 
+  //copy top trace
+  /*bblTraceTop.clear();
+  for(UINT64 id : bblTrace){
+    BblInfo* bi = bblIdInfoMap[id];
+    if(bi->isUsing){
+      bblTraceTop.push_back(id);
+    }
+  }*/
+  //cout << "Orig Trace size: " << bblTrace.size() << endl;
+  //cout << "Top trace size: " << bblTraceTop.size() << endl;
 
+  //createDCFGfromTrace(bblTrace);
 
   cout << endl << "============================================ READS ==============================================" << endl;
   //printInfoAndDelete(rInfo);
@@ -435,11 +785,26 @@ VOID Fini(INT32 code, VOID *v) {
   //printInfoAndDelete(wInfo);
   printInfo(wRes);
 
+  cout << "Inst Trace size: " << insTrace.size() << endl;
+  /*vector<ADDRINT> topTrace;
+  topTrace.reserve(insTrace.size());
+  for(ADDRINT addr : insTrace){
+    if(insAddrToBlk[addr]->isTop){
+      topTrace.push_back(addr);
+    }
+  }
+  cout << "Top Trace size: " << topTrace.size() << endl;*/
+
+  set<InsBlock*> cfg = createDCFGfromInstTrace(insTrace);
+
+  generateCode(cfg, "code.ii");
 
   deleteInfoMap(rInfo);
   deleteInfoMap(wInfo);
-  delete[] info;
-  delete[] tdata;
+  deleteInsBlockMap();
+  deleteLoops();
+  deleteBases();
+  //deleteBblMap();
 }
 
 /**
@@ -454,19 +819,29 @@ int main(int argc, char *argv[]) {
   PIN_InitLock(&lock);
   PIN_InitSymbolsAlt(IFUNC_SYMBOLS);
 
-  buf_log_lim = knobMaxLog.Value();
-  info = new MemInfo[buf_log_lim];
-  UINT64 max_threads = knobMaxThreads.Value();
-  tdata = new ThreadData[max_threads];
-  for (UINT64 i = 0; i < max_threads; ++i) {
-    tdata[i].rtnEntryCnt = 0;
-  }
   rtn_name = knobFunctionName.Value();
   read_log_en = knobRead.Value();
   write_log_en = knobWrite.Value();
 
+  insTrace.reserve(40000000);
+
+  InsBlock* begin = new InsBlock();
+  begin->id = 0;
+  begin->isTop = true;
+  insTrace.push_back(0);
+  insAddrToBlk[0] = begin;
+
+  //bblTrace.reserve(1000000);
+  //BblInfo* begin = new BblInfo();
+  //begin->id = 0;
+  //begin->entered = true;
+  //bblTrace.push_back(0);
+  //bblAddrIdMap[-1] = 0;
+  //bblIdInfoMap[0] = begin;
+
   IMG_AddInstrumentFunction(ImgCallback, NULL);
   INS_AddInstrumentFunction(Instruction, NULL);
+  //TRACE_AddInstrumentFunction(TrcCallback, NULL);
   PIN_AddFiniFunction(Fini, NULL);
 
   // Start the program, never returns
